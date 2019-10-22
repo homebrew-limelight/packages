e189bf30bd3e7564417f8175e0a5c6f088359028
132ikl <132@ikl.sh>
2019-10-22 17:41:31 -0400

    Bump version to v0.0.21

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 # -----------VERSIONS------------
-export OPENSIGHT_VERSION="0.0.20"
+export OPENSIGHT_VERSION="0.0.21"
 export OPENCV_VERSION="4.1.1"
 # -------------------------------
 
