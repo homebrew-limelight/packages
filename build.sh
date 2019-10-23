c61cc08684f032f8d3d4cf79c773e29fb8e245d5
dzil123 <5725958+dzil123@users.noreply.github.com>
2019-10-22 21:37:01 -0700

    Version v0.0.22; Change version.txt format

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 # -----------VERSIONS------------
-export OPENSIGHT_VERSION="0.0.21"
+export OPENSIGHT_VERSION="0.0.22"
 export OPENCV_VERSION="4.1.1"
 # -------------------------------
 
