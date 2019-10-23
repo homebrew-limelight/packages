151ce65674b3f6c0ab9efb025805343083dbe560
132ikl <132@ikl.sh>
2019-10-22 22:47:55 -0400

    Fix changelog generation

--- opensight/build.sh
+++ opensight/build.sh
@@ -2,7 +2,7 @@
 
 cd opensight/package
 
-git log --no-walk --tags --pretty="opensight (%D) unstable; urgency=medium%n%n  * See the full changelog at:%n  * https://github.com/opensight-cv/opensight/releases/tag/%D%n%n -- Steven Spangler <132@ikl.sh>  %cD%n" --decorate=off | sed 's/tag: //g' | sed 's/(v/(/g' | sed -r 's/([0-9]+\.[0-9]+\.[0-9]+), v[0-9]+\.[0-9]\.[0-9]+/\1/g' > debian/changelog
+git log --no-walk --tags --pretty="opensight (%S) unstable; urgency=medium%n%n  * See the full changelog at:%n  * https://github.com/opensight-cv/opensight/releases/tag/%S%n%n -- Steven Spangler <132@ikl.sh>  %cD%n" --decorate=off | sed 's/(v/(/g' | sed 's/\/v/\//g' > debian/changelog
 
 dpkg-buildpackage -us -uc --build=all
 
